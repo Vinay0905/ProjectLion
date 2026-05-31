40cb24bdc42274e06b77fa962b77ccb55cfb1ab7
Naga Vinay Avvaru <nagavinay0905@gmail.com>
2026-05-31 11:34:21 +0530

    new changes (for sending docs)

--- lion_chat/cmd/client/main.go
+++ lion_chat/cmd/client/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"log"
 	"net"
 	"os"
+	"path/filepath"
+	"strconv"
 )
 
 func main() {
@@ -28,6 +31,17 @@ func main() {
 	}()
 	input := bufio.NewScanner(os.Stdin)
 	for input.Scan() {
+		line := input.Text()
+
+		// if user types: /send path/to/file
+		if len(line) > 6 && line[:6] == "/send " {
+			path := line[6:]
+			if err := sendFile(conn, path); err != nil {
+				log.Println("sendFile error:", err)
+			}
+			continue
+		}
+
 		if _, err := fmt.Fprintln(conn, input.Text()); err != nil {
 			log.Fatal(err)
 		}
@@ -35,5 +49,27 @@ func main() {
 	if err := input.Err(); err != nil {
 		log.Println("read error from server:", err)
 	}
+
+}
+func sendFile(conn net.Conn, path string) error {
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	info, err := file.Stat()
+	name := filepath.Base(info.Name())
+	sizeStr := strconv.FormatInt(info.Size(), 10)
+	if _, err := fmt.Fprintf(conn, "FILE_META %s|%s\n", name, sizeStr); err != nil {
+		return err
+	}
+	if _, err := fmt.Fprintf(conn, "FILE_DATA %d\n", info.Size()); err != nil {
+		return err
+	}
+	// fmt.Fprintf(conn, "FILE_DATA %d\n%s|%d", len(name)+1+len(strconv.FormatInt(info.Size(), 10)), name, info.Size())
+	// fmt.Fprintf(conn, "FILE_DATA %d\n", info.Size())
+
+	_, err = io.Copy(conn, file)
+	return err
 
 }
--- lion_chat/cmd/server/main.go
+++ lion_chat/cmd/server/main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
+	"io"
 	"log"
 	"net"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 func main() {
@@ -32,6 +37,22 @@ func handleConnection(conn net.Conn) {
 	scanner := bufio.NewScanner(conn)
 	for scanner.Scan() {
 		message := scanner.Text()
+
+		if strings.HasPrefix(message, "FILE_SEND") {
+			var name string
+			var size int64
+			_, err := fmt.Sscanf(message, "FILE_SEND %s %d", &name, &size)
+			if err != nil {
+				log.Println("Bad file command", err)
+				continue
+			}
+			if err := receiveFile(conn, name, size); err != nil {
+				log.Println("recieved file error:", err)
+			}
+			continue
+
+		}
+
 		fmt.Println("message:", message)
 
 		// echo back to the same client:
@@ -43,4 +64,24 @@ func handleConnection(conn net.Conn) {
 	if err := scanner.Err(); err != nil {
 		log.Println("read error:", err)
 	}
+}
+func receiveFile(conn net.Conn, receivedName string, size int64) error {
+	safeName := filepath.Base(receivedName)
+	if safeName != receivedName || safeName == "." {
+		return errors.New("unsafe filename")
+	}
+	if size < 0 || size > 50<<20 {
+		return errors.New("File too large")
+	}
+	dst := filepath.Join("downloads", safeName)
+	if _, err := os.Stat(dst); err == nil {
+		return errors.New("file already exists")
+	}
+	out, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+	_, err = io.CopyN(out, conn, size)
+	return err
 }
